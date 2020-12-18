3787c9e99488f8aba86762a61aa5cbd4c2b7330d
Gerson Aponte <gaponter@everis.com>
2020-12-18 13:07:48 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  users = [
+    'usuario1',
+    'usuario2',
+    'usuario3',
+    'usuario4',
+    'usuario5',
+    'usuario6',
+    'usuario7',
+    'usuario8',
+    'usuario9',
+    'usuario10',
+  ];
   title = 'responsiveApp';
 }
